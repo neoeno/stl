18956edfc8102983f13188061314bce81bbc35cb
Kay Lovelace <kay@herostrat.us>
2016-08-23 00:29:42 +0100

    Work up terrible mess of parsing grammar for STL lang
    Next comes making it into a proper task and not a mess of vectors

--- src/main/scala/troglodyte/stl/STLParser.scala
+++ src/main/scala/troglodyte/stl/STLParser.scala
@@ -6,7 +6,11 @@ import scala.util.{Failure, Success}
 
 object STLParser {
   type Task = Map[String, Any]
-  case class KVPair(key: String, value: String)
+  case class STLPair(key: String, value: Any)
+  case class STLAddressRange(addressRange: String)
+  case class STLFunction(name: String, arguments: Seq[Any])
+  case class STLExpression(functions: Seq[STLFunction])
+  case class STLColumnDefinition(keyFn: STLExpression, valueFn: STLExpression)
   case class STLParserException(message: String) extends Exception(message)
 
   def parseToTask(stl: String): Task = {
@@ -18,21 +22,53 @@ object STLParser {
     }
   }
 
-  def outputToTask(pairs: Seq[KVPair]): Task = {
-    val toTaskKey = Map("NAME" -> "name", "SHEET" -> "sheet")
-    pairs.map(pair => (toTaskKey(pair.key), pair.value)).toMap
+  def outputToTask(pairs: Seq[Any]): Task = {
+    val toTaskKey = Map("NAME" -> "name", "SHEET" -> "sheet", "KEY_CELLS" -> "keyCells")
+    pairs
+      .filter(_.isInstanceOf[STLPair])
+      .map(_.asInstanceOf[STLPair])
+      .map(pair => (toTaskKey(pair.key), pair.value))
+      .toMap
+      .updated("attributes",
+        pairs
+          .filter(_.isInstanceOf[STLColumnDefinition])
+          .map(_.asInstanceOf[STLColumnDefinition])
+          .map(column => (column.keyFn, column.valueFn)))
+
   }
 
   class RealSTLParser(val input: ParserInput) extends Parser {
-    def InputLine = rule { oneOrMore(Statement).separatedBy(NewLine) ~ EOI }
+    def InputLine = rule { oneOrMore(Statement).separatedBy(oneOrMore(NewLine)) ~ EOI }
 
-    def Statement = rule { Spaces ~ Action ~ Spaces ~ Value ~ Spaces ~> KVPair }
+    def Statement = rule { KeyValueStatement | ColumnStatement }
 
-    def Action = rule { capture(oneOrMore(CharPredicate.UpperAlpha)) }
+    def KeyValueStatement = rule { Spaces ~ Action ~ Spaces ~ Value ~ Spaces ~> STLPair }
 
-    def Value = rule { StringLiteral }
+    def ColumnStatement = rule { "COLUMN" ~ Spaces ~ Expression ~ optional(Spaces ~ NewLine ~ Spaces) ~ "=>" ~ Spaces ~ Expression ~ Spaces ~> STLColumnDefinition }
+
+    def Expression = rule { oneOrMore(Function).separatedBy(Spaces ~ "|" ~ Spaces) ~> STLExpression }
+
+    def Function = rule { FunctionName ~ (ArgumentList | push(Vector())) ~> STLFunction }
+
+    def FunctionName = rule { capture(oneOrMore(CharPredicate.Alpha)) }
+
+    def ArgumentList = rule { "(" ~ zeroOrMore(Argument).separatedBy(Spaces ~ "," ~ Spaces) ~ ")" }
+
+    def Argument = rule { StringLiteral | NumberLiteral }
+
+    def Action = rule { capture("NAME" | "SHEET" | "KEY_CELLS") }
+
+    def Value = rule { StringLiteral | AddressRangeLiteral }
+
+    def AddressRangeLiteral = rule { "[" ~ capture(AddressRange) ~> STLAddressRange ~ "]"}
+
+    def AddressRange = rule { CellRef ~ ":" ~ CellRef }
+
+    def CellRef = rule { oneOrMore(CharPredicate.UpperAlpha) ~ oneOrMore(CharPredicate.Digit) }
 
     def StringLiteral = rule { '"' ~ capture(zeroOrMore(noneOf("\""))) ~ '"' }
+
+    def NumberLiteral = rule { capture(oneOrMore(CharPredicate.Digit)) }
 
     def Spaces = rule { zeroOrMore(CharPredicate(" \t")) }
 
--- src/test/scala/troglodyte/stl/STLParser$Test.scala
+++ src/test/scala/troglodyte/stl/STLParser$Test.scala
@@ -1,6 +1,7 @@
 package troglodyte.stl
 
 import org.scalatest.FunSpec
+import troglodyte.stl.STLParser.{STLAddressRange, STLExpression, STLFunction}
 
 class STLParser$Test extends FunSpec {
   describe(".parseToTask") {
@@ -9,8 +10,16 @@ class STLParser$Test extends FunSpec {
         """
           |NAME "VAP Offences By Month"
           |SHEET "Violence"
+          |KEY_CELLS [A4:A101]
+          |
+          |COLUMN fixedString("Date")
+          |    => column(0) | value
+          |COLUMN columnHeading("B3", "VAP Offences")
+          |    => column(1) | value
         """.stripMargin
-      assert(STLParser.parseToTask(stl) == Map("name" -> "VAP Offences By Month", "sheet" -> "Violence"))
+
+      // Equation stuff means this won't pass as a literal, we'll fix it later
+      assert(STLParser.parseToTask(stl).toString == "Map(name -> VAP Offences By Month, sheet -> Violence, keyCells -> STLAddressRange(A4:A101), attributes -> Vector((STLExpression(Vector(STLFunction(fixedString,Vector(Date)))),STLExpression(Vector(STLFunction(column,Vector(0)), STLFunction(value,Vector())))), (STLExpression(Vector(STLFunction(columnHeading,Vector(B3, VAP Offences)))),STLExpression(Vector(STLFunction(column,Vector(1)), STLFunction(value,Vector()))))))")
     }
   }
 }
