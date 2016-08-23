e335949dfbd40bba34585aac34262cb88b350435
Kay Lovelace <kay@herostrat.us>
2016-08-23 18:34:08 +0100

    Ultra-ugly first pass at function construction
    It works! My god!!

--- src/main/scala/troglodyte/stl/STLParser.scala
+++ src/main/scala/troglodyte/stl/STLParser.scala
@@ -1,6 +1,8 @@
 package troglodyte.stl
 
+import org.apache.poi.ss.usermodel.Cell
 import org.parboiled2._
+import troglodyte.stl.Translator.Transformer
 
 import scala.util.{Failure, Success}
 
@@ -13,28 +15,63 @@ object STLParser {
   case class STLColumnDefinition(keyFn: STLExpression, valueFn: STLExpression)
   case class STLParserException(message: String) extends Exception(message)
 
+  val stlFunctions = Map(
+    "fixedString" -> Operators.fixedString,
+    "columnHeading" -> Operators.columnHeading,
+    "column" -> Operators.columnHeading,
+    "valueOr" -> Operators.valueOr,
+    "value" -> Operators.value
+  )
+
   def parseToTask(stl: String): Task = {
     val parser = new RealSTLParser(stl.trim)
     val res = parser.InputLine.run()
     res match {
       case Success(rel) => outputToTask(rel)
       case Failure(pe:ParseError) => throw STLParserException(s"Could not parse STL: \n${parser.formatError(pe)}")
+      case _ => throw new Exception("oops!")
     }
+  }
+
+  def makeKeyFn(stlExpression: STLExpression): (Cell => String) = {
+    stlExpression.functions.map(stlFunction => {
+      (stlFunction.name match {
+        case "fixedString" => Operators.fixedString(stlFunction.arguments(0).asInstanceOf[String])
+        case "columnHeading" => Operators.columnHeading(stlFunction.arguments(0).asInstanceOf[String], stlFunction.arguments(1).asInstanceOf[String])
+        case "column" => Operators.column(stlFunction.arguments(0).asInstanceOf[String].toInt)
+        case "valueOr" => Operators.valueOr(stlFunction.arguments(0).asInstanceOf[String])
+        case "value" => Operators.value
+      }).asInstanceOf[Any => Any]
+    }).reduce((a, b) => a.compose(b)).asInstanceOf[Cell => String]
+  }
+
+  def makeValueFn(stlExpression: STLExpression): (Cell => Any) = {
+    stlExpression.functions.map(stlFunction => {
+      (stlFunction.name match {
+        case "fixedString" => Operators.fixedString(stlFunction.arguments(0).asInstanceOf[String])
+        case "columnHeading" => Operators.columnHeading(stlFunction.arguments(0).asInstanceOf[String], stlFunction.arguments(1).asInstanceOf[String])
+        case "column" => Operators.column(stlFunction.arguments(0).asInstanceOf[String].toInt)
+        case "valueOr" => Operators.valueOr(stlFunction.arguments(0).asInstanceOf[String])
+        case "value" => Operators.value
+      }).asInstanceOf[Any => Any]
+    }).reduce((a, b) => b.compose(a)).asInstanceOf[Cell => Any]
   }
 
   def outputToTask(pairs: Seq[Any]): Task = {
     val toTaskKey = Map("NAME" -> "name", "SHEET" -> "sheet", "KEY_CELLS" -> "keyCells")
-    pairs
+    val map = pairs
       .filter(_.isInstanceOf[STLPair])
       .map(_.asInstanceOf[STLPair])
       .map(pair => (toTaskKey(pair.key), pair.value))
       .toMap
+    map
+      .updated("keyCells", map("keyCells").asInstanceOf[STLAddressRange].addressRange)
       .updated("attributes",
         pairs
           .filter(_.isInstanceOf[STLColumnDefinition])
           .map(_.asInstanceOf[STLColumnDefinition])
-          .map(column => (column.keyFn, column.valueFn)))
-
+          .map(column => Transformer(makeKeyFn(column.keyFn), makeValueFn(column.valueFn)))
+          .toList)
   }
 
   class RealSTLParser(val input: ParserInput) extends Parser {
--- src/test/scala/troglodyte/stl/STLParser$Test.scala
+++ src/test/scala/troglodyte/stl/STLParser$Test.scala
@@ -1,25 +1,26 @@
 package troglodyte.stl
 
 import org.scalatest.FunSpec
-import troglodyte.stl.STLParser.{STLAddressRange, STLExpression, STLFunction}
 
 class STLParser$Test extends FunSpec {
+  val workbook = TestFactory.makeWorkbook("sheet1")(List("col1", "col2"), List(1, 2), List(3, 4))
+
   describe(".parseToTask") {
     it ("parses an STL string to a task") {
       val stl =
         """
-          |NAME "VAP Offences By Month"
-          |SHEET "Violence"
-          |KEY_CELLS [A4:A101]
+          |NAME "A Test"
+          |SHEET "sheet1"
+          |KEY_CELLS [A2:A3]
           |
-          |COLUMN fixedString("Date")
+          |COLUMN fixedString("col1")
           |    => column(0) | value
-          |COLUMN columnHeading("B3", "VAP Offences")
+          |COLUMN fixedString("col2")
           |    => column(1) | value
         """.stripMargin
 
-      // Equation stuff means this won't pass as a literal, we'll fix it later
-      assert(STLParser.parseToTask(stl).toString == "Map(name -> VAP Offences By Month, sheet -> Violence, keyCells -> STLAddressRange(A4:A101), attributes -> Vector((STLExpression(Vector(STLFunction(fixedString,Vector(Date)))),STLExpression(Vector(STLFunction(column,Vector(0)), STLFunction(value,Vector())))), (STLExpression(Vector(STLFunction(columnHeading,Vector(B3, VAP Offences)))),STLExpression(Vector(STLFunction(column,Vector(1)), STLFunction(value,Vector()))))))")
+      val records = Translator.extractRecordsByTask(STLParser.parseToTask(stl))(workbook).toList
+      assert(records == List(Map("col1" -> 1, "col2" -> 2), Map("col1" -> 3, "col2" -> 4)))
     }
   }
 }
