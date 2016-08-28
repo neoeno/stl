5d90fe163e0285169735ed3abbd6f8deb6d2e6fd
Kay Lovelace <kay@herostrat.us>
2016-08-28 17:02:24 +0100

    Make Operators methods rather than vals
    Makes for better java interop with no penalty

--- src/main/scala/troglodyte/stl/Operators.scala
+++ src/main/scala/troglodyte/stl/Operators.scala
@@ -6,28 +6,28 @@ import org.apache.poi.ss.util.CellAddress
 object Operators {
   case class SpreadsheetAssertionException(message: String) extends Exception(message)
 
-  val fixedString = (value: String) => (cell: Cell) => value
+  def fixedString(value: String) = (cell: Cell) => value
 
-  val columnHeading = (cellAddress: String, label: String) => (cell: Cell) => {
+  def columnHeading(cellAddress: String, label: String) = (cell: Cell) => {
     if (!valueOfCellAtAddress(cellAddress, cell.getSheet).contains(label)) {
       throw new SpreadsheetAssertionException(s"Column heading cell at $cellAddress did not match given value '$label'")
     }
     label
   }
 
-  val column = (colNum: Int) => (cell: Cell) =>
+  def column(colNum: Int) = (cell: Cell) =>
     Option(cell.getRow.getCell(colNum))
 
-  val valueOr = (alternative: Any) => (cell: Option[Cell]) => cell.flatMap(Spreadsheet.getCellValue).getOrElse(alternative)
+  def valueOr(alternative: Any) = (cell: Option[Cell]) => cell.flatMap(Spreadsheet.getCellValue).getOrElse(alternative)
 
-  val value = valueOr(null)
+  def value = valueOr(null)
 
-  private val valueOfCellAtAddress = (cellAddressString: String, sheet: Sheet) => {
+  private def valueOfCellAtAddress(cellAddressString: String, sheet: Sheet): Option[Any] = {
     val cellAddress = new CellAddress(cellAddressString)
     valueOfCellAtCoord(cellAddress.getRow, cellAddress.getColumn, sheet)
   }
 
-  private val valueOfCellAtCoord = (rowNum: Int, colNum: Int, sheet: Sheet) => {
+  private def valueOfCellAtCoord(rowNum: Int, colNum: Int, sheet: Sheet): Option[Any] = {
     val row = sheet.getRow(rowNum)
     if (row != null) {
       val cell = row.getCell(colNum)
